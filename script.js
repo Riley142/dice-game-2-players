1280130cd51d16e1ff14b764f52965a2bec9179f
Riley <rileyxcroft@gmail.com>
2021-02-05 16:10:03 +0000

    Few updates, still fighting with the error

--- script.js
+++ script.js
@@ -4,78 +4,92 @@ const hold = document.querySelector("#hold")
 let displayDice = document.querySelector("#displayDice")
 let displayPoints1 = document.querySelector("#displayPoints1")
 let displayPoints2 = document.querySelector("#displayPoints1")
+let currentPoints1 = document.querySelector("#currentPoints1")
+let currentPoints2 = document.querySelector("#currentPoints2")
 let playerStatus1 = document.querySelector("#playerStatus1")
 let playerStatus2 = document.querySelector("#playerStatus2")
 let pointsCounter1 = 0;
 let pointsCounter2 = 0;
-let cp1 = document.getElementById('playerStatus1').classList.add('currentP1');
 
-
+let playerOne = true;
 
 roll.style.display = "none";
 hold.style.display = "none";
 
 newGame.addEventListener("click", (event) => {
-    let cp1 = document.getElementById('playerStatus1').classList.add('currentP1');
     roll.style.display = "block";
     hold.style.display = "block";
+    newGame.style.display = "none"; 
 });
 
 roll.addEventListener("click", (event) => {
-    playOne();
+    if (playerOne === true) {
+        playOne();
+        console.log("player one playing");
+    } else if (playerOne === false) {
+        playerOne = false;
+        console.log("player two playing");
+        playOne();
+    }  
 });
 
-
 hold.addEventListener("click", (event) => {
-    if (cp1.classList.contains(currentP1)) {
+    if (playerOne === true) {
+        console.log("should start player two?");
+    } else if (playerOne === false) {
+        console.log("should start player one?");
         playTwo();
-    }  else {
-        playOne();
     }
 });
 
+
+
 function startAgain() {
-    newGame.addEventListener("click", (event) => {
-        roll.style.display = "block";
-        hold.style.display = "block";
-    });
+    window.location.reload();
 };
 
 
 function playOne() {
-    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    let cp1 = document.getElementById('playerStatus1').classList.add('currentP1');
+    let cp2 = document.getElementById('playerStatus2').classList.remove('currentP2');
 
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    
     if (diceRoll === 1) {
         displayDice.src = "img/dice1.png"
-        displayPoints1.innerText = "Game over";
-        roll.style.display = "none";
-
+        currentPoints1.innerText = diceRoll;
+        displayPoints1.innerText = "1";
+        setTimeout(startAgain, 1500);
     } else if (diceRoll === 2) {
         displayDice.src = "img/dice2.png"
         displayPoints1.innerText = pointsCounter1;
+        currentPoints1.innerText = diceRoll;
         pointsCounter1 += diceRoll;
     } else if (diceRoll === 3) {
         displayDice.src = "img/dice3.png"
         displayPoints1.innerText = pointsCounter1;
+        currentPoints1.innerText = diceRoll;
         pointsCounter1 += diceRoll;
     } else if (diceRoll === 4) {
         displayDice.src = "img/dice4.png"
         displayPoints1.innerText = pointsCounter1;
+        currentPoints1.innerText = diceRoll;
         pointsCounter1 += diceRoll;
     } else if (diceRoll === 5) {
         displayDice.src = "img/dice5.png"
         displayPoints1.innerText = pointsCounter1;
+        currentPoints1.innerText = diceRoll;
         pointsCounter1 += diceRoll;
     } else {
         displayDice.src = "img/dice6.png"
         displayPoints1.innerText = pointsCounter1;
+        currentPoints1.innerText = diceRoll;
         pointsCounter1 += diceRoll;
     }
 
-
     if (pointsCounter1 >= 20) {
         displayPoints1.innerText = "You won!";
-        pleyOne();
+        setTimeout(startAgain, 1500);
     }
 
     console.log(pointsCounter1);
@@ -83,39 +97,48 @@ function playOne() {
 
 
 function playTwo() {
+    let cp1 = document.getElementById('playerStatus1').classList.remove('currentP1');
+    let cp2 = document.getElementById('playerStatus2').classList.add('currentP2');
+    
     const diceRoll = Math.floor(Math.random() * 6) + 1;
 
     if (diceRoll === 1) {
         displayDice.src = "img/dice1.png"
-        displayPoints1.innerText = "Game over";
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = "Game over";
         roll.style.display = "none";
-
+        setTimeout(startAgain, 1500);
     } else if (diceRoll === 2) {
         displayDice.src = "img/dice2.png"
-        displayPoints1.innerText = pointsCounter2;
-        pointsCounter1 += diceRoll;
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = pointsCounter2;
+        pointsCounter2 += diceRoll;
     } else if (diceRoll === 3) {
         displayDice.src = "img/dice3.png"
-        displayPoints1.innerText = pointsCounter2;
-        pointsCounter1 += diceRoll;
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = pointsCounter2;
+        pointsCounter2 += diceRoll;
     } else if (diceRoll === 4) {
         displayDice.src = "img/dice4.png"
-        displayPoints1.innerText = pointsCounter2;
-        pointsCounter1 += diceRoll;
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = pointsCounter2;
+        pointsCounter2 += diceRoll;
     } else if (diceRoll === 5) {
         displayDice.src = "img/dice5.png"
-        displayPoints1.innerText = pointsCounter2;
-        pointsCounter1 += diceRoll;
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = pointsCounter2;
+        pointsCounter2 += diceRoll;
     } else {
         displayDice.src = "img/dice6.png"
-        displayPoints1.innerText = pointsCounter2;
-        pointsCounter1 += diceRoll;
+        currentPoints2.innerText = diceRoll;
+        displayPoints2.innerText = pointsCounter2;
+        pointsCounter2 += diceRoll;
     }
 
 
     if (pointsCounter2 >= 20) {
         displayPoints2.innerText = "You won!";
-        pleyOne();
+        setTimeout(startAgain, 1500);
     }
 
     console.log(pointsCounter2);
